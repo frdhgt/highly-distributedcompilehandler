fe56956ace67582b83ade93506ea51f28914f007
frdhgt <csjcharlesjazzy@hotmail.com>
2024-05-10 05:31:02 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const isPalindrome = (str) => str === str.split("").reverse().join("");
+function findPeakElement(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] < nums[mid + 1]) left = mid + 1;
+    else right = mid;
+  }
+  return left;
+}
